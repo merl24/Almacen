d9690d8b74baa17102c431a3096635994ce5d48a
YVega <merl.veda24@gmail.com>
2025-03-27 00:26:10 -0500

    first commit

--- Servicios/settings.py
+++ Servicios/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
